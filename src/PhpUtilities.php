1b02d1688a819940727dcfe35bf9c6693fd8b3f5
bhavingajjar <gajjarbhavin22@gmail.com>
2021-03-02 11:27:56 +0530

    add is english function

--- src/PhpUtilities.php
+++ src/PhpUtilities.php
@@ -4,5 +4,46 @@ namespace Bhavingajjar\PhpUtilities;
 
 class PhpUtilities
 {
-    // Build your next great package.
+    /*
+     * ================
+     * String Functions
+     * ================
+     * */
+    /**
+     * isStringEnglish will check the input string is in english or not
+     * @param $str
+     * @return bool
+     */
+    public static function isStringEnglish($str): bool
+    {
+        if (strlen($str) !== strlen(utf8_decode($str)))
+            return false;
+        else
+            return true;
+    }
+    /*
+     * ================
+     * String Functions
+     * ================
+     * */
+
+    /*
+     * ================
+     * Number Functions
+     * ================
+     * */
+    /**
+     * parseNumber will get input as value as string or number and return float or integer number
+     * @param $value
+     * @return float|int
+     */
+    public static function parseNumber($value)
+    {
+        return ($value == (int)$value) ? (int)$value : (float)$value;
+    }
+    /*
+     * ================
+     * Number Functions
+     * ================
+     * */
 }
